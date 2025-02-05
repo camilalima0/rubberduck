2778e2477fe518fbb85643f8f486f54d70e79430
Camila Claudino de Lima <144016007+camilalima0@users.noreply.github.com>
2025-02-05 22:01:29 +0000

    Page register.html and layout.html created

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/', methods = ["GET", "POST"])
-def hello():
+def home():
     return render_template('index.html')
+
+@app.route('/register_link', methods = ["GET", "POST"])
+def register_link():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
