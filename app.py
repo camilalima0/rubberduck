285e26bb7da0e08de30817f809f5451a345f2288
Camila Claudino de Lima <144016007+camilalima0@users.noreply.github.com>
2025-02-07 21:26:51 +0000

    register and login pages created, trying to create fiction.html

--- app.py
+++ app.py
@@ -10,5 +10,16 @@ def home():
 def register_link():
     return render_template('register.html')
 
+@app.route('/login_link', methods = ["GET", "POST"])
+def login_link():
+    return render_template('login.html')
+
+#configurar envio do login
+
+@app.route('/fiction', methods = ["GET"])
+def fiction():
+    #filtrar no banco de dados para exibir os livros do genero ficcao
+    return render_template('fiction.html')
+
 if __name__ == '__main__':
     app.run(debug = True)
