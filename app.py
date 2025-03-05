f359128c1348292f148a7ff3903cf815bef3d91e
Camila Claudino de Lima <144016007+camilalima0@users.noreply.github.com>
2025-03-05 12:04:37 +0000

    starting backend - google books api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, app
+from flask import Flask, render_template, app, request
+import requests
 
 app = Flask(__name__)
 
@@ -38,9 +39,64 @@ def cart():
 def payment():
     return render_template('payment.html')
 
+API_GOOGLE_BOOKS = 'AIzaSyCLAhnzzjKGAwpUu7N6DLUJpgzTPsGQeyQ'
+
+# Função para buscar informações sobre um livro
+def search(query):
+    # URL base da API do Google Books
+    url = f'https://www.googleapis.com/books/v1/volumes?q={query}&key={API_GOOGLE_BOOKS}'
+
+    # requesting the API
+    response = requests.get(url)
+
+    #initializing data variable:
+    data = None
+
+    # status 200 = request succeded
+    if response.status_code == 200:
+        print(f"Dados retornados da API: {data}")  # Log dos dados retornados
+        # Converts the answaer to JSON
+        data = response.json()
+        books= []
+
+        # is there items into the answer?
+        if 'items' in data:
+            for item in data['items']:
+                volume_info = item['volumeInfo']
+
+                book = {
+                    # Extrair informações do livro
+                'title': volume_info.get('title', 'Title not found'),
+                'authors': volume_info.get('authors', ['Unknown author']),
+                'publishedDate': volume_info.get('publishedDate', 'Data de publicação não disponível'),
+                'description': volume_info.get('description', 'Descrição não disponível'),
+                'cover': volume_info.get('imageLinks', {}).get('thumbnail', 'Imagem não disponível')
+                }
+                books.append(book)
+
+        return books
+    else:
+        return None
+
+# Rota para a busca de livros
+@app.route('/search-book', methods=['GET'])
+def search_books():
+    query = request.args.get('query')  # Pega o valor da query
+    if query:
+        books = search(query)  # Chama a função para buscar o livro
+
+        if books:
+            return render_template('search_results.html', books=books)
+        else:
+            return render_template('search_results.html', books=[], error="Nothing found.")
+    else:
+        return render_template('search_results.html', livros=[], error="Please, type a query.")
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0')
+
+
+
 
 #invenção de moda:
 #colocar icone de numero de itens no carrinho
