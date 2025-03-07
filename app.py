397ccc87ca505bf3363aace8f9b60b4a079670e5
Camila Claudino de Lima <144016007+camilalima0@users.noreply.github.com>
2025-03-07 00:03:15 +0000

    search by genre set up, need to fix some bugs. update books in homepage

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ def login_link():
 
 #configurar envio do login
 
-@app.route('/scifi', methods = ["GET"]) #app.route /gender > e pega no banco de dados o nome do genero que foi clicado e exibe os resultados
+@app.route('/scifi', methods = ["GET"]) #app.route /genre > e pega no banco de dados o nome do genre que foi clicado e exibe os resultados
 def scifi():
-    #filtrar no banco de dados para exibir os livros do genero ficcao
+    #filtrar no banco de dados para exibir os books do genre ficcao
     return render_template('sci-fi.html')
 
 @app.route('/bookpage', methods = ["GET"])
@@ -78,7 +78,7 @@ def search(query):
     else:
         return None
 
-# Rota para a busca de livros
+# Rota para a busca de books
 @app.route('/search-book', methods=['GET'])
 def search_books():
     query = request.args.get('query')  # Pega o valor da query
@@ -90,38 +90,34 @@ def search_books():
         else:
             return render_template('search_results.html', books=[], error="Nothing found.")
     else:
-        return render_template('search_results.html', livros=[], error="Please, type a query.")
+        return render_template('search_results.html', books=[], error="Please, type a query.")
     
-#function to filter by gender:
-def search(query):
-    # URL base da API do Google Books
-    url = f'https://www.googleapis.com/books/v1/volumes?q={query}&key={API_GOOGLE_BOOKS}'
+# #function to filter by genre:
+def search_subject(query):
+    # URL base da API do Google Books, com uso do parâmetro "subject" (assunto)
+    url = f'https://www.googleapis.com/books/v1/volumes?q=subject:{query}&key={API_GOOGLE_BOOKS}'
 
-    # requesting the API
+    # Requisição para a API
     response = requests.get(url)
 
-    #initializing data variable:
+    # Inicializando a variável de dados:
     data = None
 
-    # status 200 = request succeded
     if response.status_code == 200:
         print(f"Dados retornados da API: {data}")  # Log dos dados retornados
-        # Converts the answaer to JSON
         data = response.json()
-        books= []
+        books = []
 
-        # is there items into the answer?
         if 'items' in data:
             for item in data['items']:
                 volume_info = item['volumeInfo']
 
                 book = {
-                    # Extrair informações do livro
-                'title': volume_info.get('title', 'Title not found'),
-                'authors': volume_info.get('authors', ['Unknown author']),
-                'publishedDate': volume_info.get('publishedDate', 'Data de publicação não disponível'),
-                'description': volume_info.get('description', 'Descrição não disponível'),
-                'cover': volume_info.get('imageLinks', {}).get('thumbnail', 'Imagem não disponível')
+                    'title': volume_info.get('title', 'Título não encontrado'),
+                    'authors': volume_info.get('authors', ['Autor desconhecido']),
+                    'publishedDate': volume_info.get('publishedDate', 'Data de publicação não disponível'),
+                    'description': volume_info.get('description', 'Descrição não disponível'),
+                    'cover': volume_info.get('imageLinks', {}).get('thumbnail', 'Imagem não disponível')
                 }
                 books.append(book)
 
@@ -129,19 +125,6 @@ def search(query):
     else:
         return None
 
-# Rota para a busca de livros
-@app.route('/search-book', methods=['GET'])
-def search_books():
-    query = request.args.get('query')  # Pega o valor da query
-    if query:
-        books = search(query)  # Chama a função para buscar o livro
-
-        if books:
-            return render_template('search_results.html', books=books)
-        else:
-            return render_template('search_results.html', books=[], error="Nothing found.")
-    else:
-        return render_template('search_results.html', livros=[], error="Please, type a query.")
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0')
